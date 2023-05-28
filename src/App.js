9bde2b637c4f56436172153acb0603a5da54f71a
RemonAnsary <rasel15-10514@diu.edu.bd>
2023-05-28 23:49:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,148 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios"
+import React, { useState, useEffect } from "react"
 
 function App() {
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [email, setEmail] = useState("")
+  const [recipientEmail, setRecipientEmail] = useState("")
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/bulkemail")
+      .then((res) => {
+        setData(res.data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    axios
+      .post("http://127.0.0.1:8000/api/bulkemail", { email })
+      .then((res) => {
+        console.log("Email added successfully")
+
+        axios
+          .get("http://127.0.0.1:8000/api/bulkemail")
+          .then((res) => {
+            setData(res.data)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    setEmail("")
+  }
+
+  const handleSendEmail = (e) => {
+    e.preventDefault()
+
+    axios
+      .post("http://127.0.0.1:8000/api/sendemail", {
+        receiver_email: recipientEmail,
+      })
+      .then((res) => {
+        console.log("Email sent to the user")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    setRecipientEmail("")
+  }
+
+  const handleSendBulkEmail = () => {
+    axios
+      .get("http://127.0.0.1:8000/api/sendbulkemail")
+      .then((res) => {
+        console.log("Email sent to all email addresses")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <h3>Create Bulk List</h3>
+        <table className="table table-striped table-bordered table-hover">
+          <thead className="thead-dark">
+            <tr>
+              <th>Email</th>
+              <th>Created At</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                <td>{item.email}</td>
+                <td>{item.created_at.split("T")[0]}</td>
+              </tr>
+            ))}
+            <button
+              className="btn btn-success mt-3"
+              onClick={handleSendBulkEmail}
+              disabled={data.length === 0}
+            >
+              Send Email to All
+            </button>
+          </tbody>
+        </table>
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label>Add Email to Bulk List</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+      <br />
+      <br />
+      <div>
+        <h3>Send Email to Single User</h3>
+
+        <form onSubmit={handleSendEmail}>
+          <div className="form-group">
+            <label>Recipient Email</label>
+            <input
+              type="email"
+              className="form-control"
+              placeholder="Enter recipient email"
+              value={recipientEmail}
+              onChange={(e) => setRecipientEmail(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Send Email
+          </button>
+        </form>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
